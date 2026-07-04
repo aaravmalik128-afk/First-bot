2d783e8e63a13c8c57a5e7db802b92838cc38bb9
aaravmalik128-afk <aaravmalik128@gmail.com>
2026-07-04 15:16:36 +0530

    Update userModel.js

--- userModel.js
+++ userModel.js
@@ -1,28 +1,32 @@
-const mongoose = require("mongoose");
+const mongoose = require('mongoose');
 
-const userSchema = new mongoose.Schema(
-{
-    userId: {
+const userSchema = new mongoose.Schema({
+    telegramId: {
         type: Number,
         required: true,
-        unique: true
-    },
-    firstName: {
-        type: String,
-        default: ""
-    },
-    lastName: {
-        type: String,
-        default: ""
+        unique: true, // ताकि एक यूजर बार-बार डुप्लिकेट न हो
+        index: true // फास्ट सर्चिंग के लिए इंडेक्सिंग
     },
     username: {
         type: String,
-        default: ""
+        default: null
+    },
+    firstName: {
+        type: String,
+        required: true
+    },
+    lastName: {
+        type: String,
+        default: null
     },
     joinedAt: {
         type: Date,
         default: Date.now
     }
+}, {
+    timestamps: true // इससे createdAt और updatedAt ऑटोमैटिक मैनेज होंगे
 });
 
-module.exports = mongoose.model("User", userSchema);
+const User = mongoose.model('User', userSchema);
+
+module.exports = User;
